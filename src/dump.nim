e4dfead595cae25d9477b605f5ecfb6a3b8e2d5c
inv2004 <inv2004@gmail.com>
2021-02-10 02:46:33 +0300

    trim dump stdin

--- src/dump.nim
+++ src/dump.nim
@@ -8,7 +8,10 @@ let toEval = if isatty(stdin): "" else: readAll(stdin)
 let db = newDB()
 if toEval.len > 0:
   try:
-    for q in toEval.split(";"):
+    for s in toEval.split(";"):
+      let q = s.strip(true, true)
+      if q.len == 0:
+        continue
       echo "Exe: ", q
       for r in db.db.rows(sql(q)):
         echo r
